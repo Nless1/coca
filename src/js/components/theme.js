d8fe92eea6faf261744e00107db6197976224321
Nless1 <lobkovskiy.danil@mail.ru>
2024-05-09 00:46:28 +0500

    Created header

---
+++ src/js/components/theme.js
@@ -0,0 +1,21 @@
+export const useTheme = () => {
+  const themeTarget = document.querySelector('[data-theme]');
+  const themeSwitcher = document.querySelector('[data-theme-switcher]');
+
+  const theme = localStorage.getItem('theme') || 'light';
+  themeTarget.dataset.theme = theme;
+
+  if (theme === 'dark') {
+    themeSwitcher.checked = true;
+  }
+
+  themeSwitcher.addEventListener('click', () => {
+    if (themeTarget.dataset.theme === 'light') {
+      themeTarget.dataset.theme = 'dark';
+      localStorage.setItem('theme', 'dark');
+    } else {
+      themeTarget.dataset.theme = 'light';
+      localStorage.setItem('theme', 'light');
+    }
+  });
+};
