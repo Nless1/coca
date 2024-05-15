a1ebbd775fbba39626a514e79d7592b25d273ac8
Nless1 <lobkovskiy.danil@mail.ru>
2024-05-15 23:50:57 +0500

    Created insight section and added swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import '../assets/scss/main.scss';
 
 // components
-import { scrollWindow } from './components/scroll-header';
+import { scrollWindow } from './components/scroll-header.js';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { useInsightSlider } from './components/slider.js';
 
 scrollWindow();
 useTheme();
 useBurger();
+useInsightSlider();
