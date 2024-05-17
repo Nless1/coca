bf17ce83a9919abd4d3d35580c868e5ce3c64936
Nless1 <lobkovskiy.danil@mail.ru>
2024-05-17 18:50:57 +0500

    Added fade effect

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,6 +1,7 @@
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 import { Autoplay } from 'swiper/modules';
+import { EffectFade } from 'swiper/modules';
 import 'swiper/css';
 
 export const useInsightSlider = () => {
@@ -33,7 +34,7 @@ export const usePartnersMobileSlider = () => {
 
 export const useTestimonialsSlider = () => {
   new Swiper('.testimonials__slider', {
-    modules: [Navigation],
+    modules: [Navigation, EffectFade],
     slidesPerView: 'auto',
     spaceBetween: 22,
     loop: true,
@@ -41,5 +42,6 @@ export const useTestimonialsSlider = () => {
       prevEl: '.testimonials__btn--prev',
       nextEl: '.testimonials__btn--next',
     },
+    effect: 'fade'
   });
 };
