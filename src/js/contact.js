5e2c2ea6216124c536fb51f88dc72bd31cee1732
Nless1 <lobkovskiy.danil@mail.ru>
2024-05-21 22:51:34 +0500

    Created contact:hero and added contact:partners

---
+++ src/js/contact.js
@@ -0,0 +1,17 @@
+import '../assets/scss/contact.scss';
+
+// components
+import { scrollWindow } from './components/scroll-header.js';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useInsightSlider, usePartnersMobileSlider, useTestimonialsSlider } from './components/slider.js';
+import { useHoverAccordion } from './components/hover-accordion.js';
+
+
+scrollWindow();
+useTheme();
+useBurger();
+useInsightSlider();
+usePartnersMobileSlider();
+useTestimonialsSlider();
+useHoverAccordion();
--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './src/index.html',
+        pricing: './src/pricing.html',
+        contact: './src/contact.html',
       },
     },
   },
