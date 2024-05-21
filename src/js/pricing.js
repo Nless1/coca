60d160cc83d79100576f15c1baa867f5cce9be25
Nless1 <lobkovskiy.danil@mail.ru>
2024-05-21 16:21:35 +0500

    Created pricing:hero section

---
+++ src/js/pricing.js
@@ -0,0 +1,15 @@
+import '../assets/scss/pricing.scss';
+
+// components
+import { scrollWindow } from './components/scroll-header.js';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useHoverAccordion } from './components/hover-accordion.js';
+import { usePriceSwitcher } from './components/pricing/priceSwticher.js';
+
+
+scrollWindow();
+useTheme();
+useBurger();
+useHoverAccordion();
+usePriceSwitcher();
