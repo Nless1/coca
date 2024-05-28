1296fa87daded38fff86ea994b16f2fb390d2742
Nless1 <lobkovskiy.danil@mail.ru>
2024-05-28 14:17:31 +0500

    Added about:hero section

--- src/js/about.js
+++ src/js/about.js
@@ -1,15 +1,13 @@
-import '/scss/pricing.scss';
+import '/scss/about.scss';
 
 // components
 import { scrollWindow } from './components/scroll-header.js';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useHoverAccordion } from './components/hover-accordion.js';
-import { usePriceSwitcher } from './components/pricing/priceSwticher.js';
 
 
 scrollWindow();
 useTheme();
 useBurger();
 useHoverAccordion();
-usePriceSwitcher();
